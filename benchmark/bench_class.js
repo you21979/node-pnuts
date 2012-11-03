d18b95d6d3ce258d02d35c35b722866f5ec7a2bc
you21979 <you2197901@gmail.com>
2012-11-03 13:26:22 +0900

    add fsm

---
+++ benchmark/bench_class.js
@@ -0,0 +1,74 @@
+var PNuts = require('..'),
+    Singleton = PNuts.util.Singleton,
+    Class = PNuts.Class;
+
+var assert = require('assert');
+
+var Def = function(){
+    this.aaa_ = 'test';
+}
+
+var Simple = Class({
+    initialize : function(){
+        this.aaa_ = 'test';
+    },
+    get : function(){
+        return this.aaa_;
+    },
+});
+var Complex1 = Simple.extends({});
+var Complex2 = Complex1.extends({});
+var Complex3 = Complex2.extends({});
+var Complex4 = Complex3.extends({});
+var Complex5 = Complex4.extends({});
+var Complex6 = Complex5.extends({});
+var Complex7 = Complex6.extends({});
+
+var S = Singleton(Simple,{});
+var C7 = Singleton(Complex7,{});
+
+[
+function(){
+    var x = {};
+},
+function(){
+    var x = new Def();
+},
+function(){
+    var x = new Simple();
+},
+function(){
+    var x = new Complex1();
+},
+function(){
+    var x = new Complex2();
+},
+function(){
+    var x = new Complex3();
+},
+function(){
+    var x = new Complex4();
+},
+function(){
+    var x = new Complex5();
+},
+function(){
+    var x = new Complex6();
+},
+function(){
+    var x = new Complex7();
+},
+function(){
+    var x = C7.get();
+},
+function(){
+    var x = S.get();
+}
+].forEach(function(fnc){
+    var TID = 'SUM';
+    console.time(TID);
+    for(var i=0;i<10000000;++i){
+        fnc();
+    };
+    console.timeEnd(TID);
+});
