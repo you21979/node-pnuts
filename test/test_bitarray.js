1f5652ac5dc62e11ac4c566f10d09c4934115307
you21979 <you2197901@gmail.com>
2012-12-21 15:07:12 +0900

    method rename

--- test/test_bitarray.js
+++ test/test_bitarray.js
@@ -7,26 +7,26 @@ var assert = require('assert');
 function(MAX){
     var copy = new BitArray(MAX);
     for(var i=0;i<MAX;++i){
-        copy.set(i);
-        copy.clear(i);
+        copy.on(i);
+        copy.off(i);
     }
 },
 function(MAX){
     var flags = new BitArray(1000);
-    assert(flags.check(1000) === false);//フラグ数オーバー
-    assert(flags.get(100) === false);
-    flags.set(100);
-    assert(flags.get(100) === true);
-    flags.clear(100);
-    assert(flags.get(100) === false);
-    flags.clear(101);
-    assert(flags.get(101) === false);
-    flags.clear(101);
-    assert(flags.get(101) === false);
-    flags.set(999);
+    assert(flags.isRange(1000) === false);//フラグ数オーバー
+    assert(flags.is(100) === false);
+    flags.on(100);
+    assert(flags.is(100) === true);
+    flags.off(100);
+    assert(flags.is(100) === false);
+    flags.off(101);
+    assert(flags.is(101) === false);
+    flags.off(101);
+    assert(flags.is(101) === false);
+    flags.on(999);
     var copy = new BitArray(1000);
     copy.fromString(flags.toString());
-    assert(copy.get(999) === true);
+    assert(copy.is(999) === true);
     assert(flags.toString() === copy.toString());
 }
 ].forEach(function(fnc){
