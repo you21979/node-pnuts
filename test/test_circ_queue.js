ac93672eca7ef010f0c58496d9466fe374b1d925
you21979 <you2197901@gmail.com>
2012-12-23 01:19:15 +0900

    circuit調整

--- test/test_circ_queue.js
+++ test/test_circ_queue.js
@@ -2,22 +2,23 @@ var PNuts = require('..'),
     CircQueue = PNuts.collection.CircQueue;
 var assert = require('assert');
 
+var MAX = 10000000;
+
 [function(){
     var circ = new CircQueue();
     var i = 0;
-    circ.enqueue(i++);
-    circ.enqueue(i++);
-    circ.enqueue(i++);
-    circ.enqueue(i++);
-    circ.enqueue(i++);
-    circ.enqueue(i++);
+    for(i = 0; i<MAX; ++i){
+        circ.enqueue(i);
+    }
     var max = i;
 
-    for(var i=0; i<100; ++i){
+    for(var i=0; i<MAX; ++i){
         circ.exec(function(data){
             assert(data === i % max);
         });
     }
+    circ.finalize();
+console.log(circ)
 }].forEach(function(f){
     f();
 });
